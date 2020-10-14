4b043dfac0a451faf0ab4a1fe355e280a4deffc8
Marcell L <mcl.wall@gmail.com>
2020-10-14 22:15:31 +0200

    workshop animations - some progress

--- src/components/Workshop-Info-Banner.vue
+++ src/components/Workshop-Info-Banner.vue
@@ -16,41 +16,50 @@
     
     <!-- More Info Layer -->
     <div class="layer more-info-layer"
-        :class="moreInfoTeaser ? ' teaser' : ''"
-        @click="toggleMoreInfo"
-        @mouseover="turnOnMoreInfoTeaser" 
-        @mouseout="turnOffMoreInfoTeaser">
-      <div class="arrow-right" >
-      </div>
-      <div class="arrow-description ml-3">
-          Mehr <br> Info
-      </div>
-      <div class="content markdown-body pr-5">
-        <h4>
-          Der Workshop, der dir deine Stärken zeigt.
-        </h4>
-        <p>Im Workshop Stärken entdecken finden junge Menschen ihre persönlichen Stärken und erkennen Möglichkeiten, diese selbstbewusst in Schule, Freizeit und Beruf einzusetzen.</p>
-      </div>
+        :class="showMoreInfo ? ' show' : ''">
+        <div class="clicker" 
+        :class="showMoreInfo ? 'd-none' : ''"
+        @click="toggleMoreInfo">
+
+        </div>
+        <div class="content">
+          <div class="arrow-right" >
+          </div>
+          <div class="arrow-description ml-3" v-show="!showMoreInfo">
+              {{!showAllDetails ? 'Mehr Info' : 'Zurück'}}
+          </div>
+          
+          <div v-if="showMoreInfo" class="content markdown-body mt-3 pl-4 pr-1">
+            <h4>
+              Der Workshop, der dir deine Stärken zeigt.
+            </h4>
+            <p>Im Workshop Stärken entdecken finden junge Menschen ihre persönlichen Stärken und erkennen Möglichkeiten, diese selbstbewusst in Schule, Freizeit und Beruf einzusetzen.</p>
+          </div>
+          <b-button-close class="ml-2 mr-4" @click="toggleMoreInfo">
+            x
+          </b-button-close>
+        </div>
     </div>
     
     <!-- All Details Layer -->
-    <div class="layer all-details-layer"
-      :class="allDetailsTeaser ? ' teaser' : ''"
-      @mouseover="turnOnAllDetailsTeaser"
-      @mouseout="turnOffAllDetailsTeaser"
+    <div class="layer all-details-layer py-4"
+      :class="showAllDetails ? ' show' : ''"
       @click="toggleAllDetails">
       <div class="arrow-right">
       </div>
-      <div class="arrow-description ml-3">
+      <div class="arrow-description ml-3" v-show="!showAllDetails">
           Mehr <br> Details
       </div>
-      <div v-if="showAllDetails" class="content markdown-body pr-5">
-        <p>Im Workshop Stärken entdecken ermöglichen wir jungen Menschen, die eigenen Stärken zu entdecken und so Vertrauen in sich selbst zu gewinnen. Denn das Bewusstsein über die eigenen Stärken hilft dabei, mutige und selbstbewusste Entscheidungen zu treffen.
+        <div v-if="showAllDetails" class="content markdown-body mt-3 pl-4 pr-5">
+          <p>Im Workshop Stärken entdecken ermöglichen wir jungen Menschen, die eigenen Stärken zu entdecken und so Vertrauen in sich selbst zu gewinnen. Denn das Bewusstsein über die eigenen Stärken hilft dabei, mutige und selbstbewusste Entscheidungen zu treffen.
 
-Mit diversen interaktiven Methoden wie
-- Stärken-Brainstorming | - Phantasiereise | - Selbst- und Fremdfeedback
-sowie dazu passenden, altersadequaten Arbeitsmaterialien fördern wir Schlüsselkompetenzen wie Selbsterfahrung, Empathie, Team- und Kommunikationsfähigkeit.</p>
-      </div>
+  Mit diversen interaktiven Methoden wie
+  - Stärken-Brainstorming | - Phantasiereise | - Selbst- und Fremdfeedback
+  sowie dazu passenden, altersadequaten Arbeitsmaterialien fördern wir Schlüsselkompetenzen wie Selbsterfahrung, Empathie, Team- und Kommunikationsfähigkeit.</p>
+        </div>
+        <b-button-close class="ml-2 mr-4" @click="toggleAllDetails">
+        x
+      </b-button-close>
     </div>
   </div>
 </template>
@@ -63,7 +72,8 @@ export default {
       showAllDetails: false,
       moreInfoTeaser: false,
       allDetailsTeaser: false,
-      transitioning: false
+      transitioning: false,
+      coverOpen: true
     }
   },
   props: {
@@ -73,11 +83,18 @@ export default {
   },
   methods: {
     toggleMoreInfo() {
+      // Show More Info is not open
+      if (!this.showMoreInfo) {
+        if (this.showAllDetails)
+          this.showAllDetails = !this.showAllDetails
+      }
       this.showMoreInfo = !this.showMoreInfo
     },
     toggleAllDetails() {
-      this.showAllDetails = !this.showAllDetails
-      this.showMoreInfo = !this.showMoreInfo
+      if (!this.showAllDetails) {
+        this.showAllDetails = !this.showAllDetails
+        this.showMoreInfo = !this.showMoreInfo
+      }
     },
     turnOnMoreInfoTeaser() {
       if (!this.showAllDetails && !this.showMoreInfo) {
@@ -100,12 +117,37 @@ export default {
 </script>
 
 <style lang="scss">
+  .clicker,
+  .content {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+  .clicker {
+    z-index: 100;
+  }
+  .content {
+
+
+  }
   .workshop-info-banner {
     display: flex;
     overflow: hidden;
     
+    .more-info-layer,
+    .all-details-layer {
+      position: relative;
+      cursor: pointer;
+      &.show {
+        cursor: inherit;
+      }
+      button.close,
+      .content {
+        align-self: flex-start;
+      }
+    }
+
     .cover-layer {
-      transition: .7s ease-in-out;
       width: 80%;
       display: grid;
       grid-row: 1 / 1;
@@ -122,61 +164,43 @@ export default {
       }
     }
     .more-info-layer {
-      transition: .7s ease-in-out;
-      cursor: pointer;
       width: 20%;
-      display: flex;
       grid-row: 1 / 1;
       grid-column: 2 / 3;
-      align-items: center;
-
+      
     }
     .all-details-layer {
-      transition: .7s ease-in-out;
-      cursor: pointer;
       width: 0%;
-      display: flex;
-      align-items: center;
     }
-    .arrow-description {
-      transition: .3s ease-in-out;
-    }
-    .content {
-      transition: .6s ease-out;
-      opacity: 0;
-    }
+    
     &.show-more-info {
-      .cover-layer,
+      .cover-layer {
+        width: 48%;
+      }
       .more-info-layer {
-        width: 40%;
-        .content {
-          opacity: 1;
-        }
+        width: 36%;
       }
       .all-details-layer {
-        width: 20%;
+        width: 16%;
 
       }
     }
     &.show-all-details {
       .cover-layer {
-        width: 20%;
+        width: 48%;
       }
       .more-info-layer {
-        width: 5%;
+        width: 12%;
       }
       .all-details-layer {
-        width: 75%;
-        .content {
-          opacity: 1;
-        }
+        width: 40%;
       }
     }
     .cover-layer {
       padding-top: 2em;
     }
 
-    // Show Cursors
+    // Show Arrow
     &.show-more-info {
       .more-info-layer {
         .arrow-description {
@@ -191,23 +215,32 @@ export default {
         }
       }
     }
+
+    // Transitions
+    .cover-layer,
+    .more-info-layer,
+    .all-details-layer {
+      transition: .7s ease-in-out;
+    }
+    .arrow-description {
+      transition: .3s ease-in-out;
+    }
+    .content {
+      transition: .6s ease-out;
+    }
+    .show-content-enter-active {
+      transition: opacity .8s ease-in;
+    }
+    .show-content-leave-active {
+      transition: opacity .1s ease-in;
+    }
+    .show-content-enter {
+      opacity: 1;
+    }
+    .show-content-leave-to {
+      opacity: 0;
+    }
     // Hover Effects
-    &.more-info-teaser {
-      .cover-layer {
-        width: 78%;
-      }
-      .more-info-layer {
-        width: 22%;
-      }
-    }
-    &.all-details-teaser {
-      .more-info-layer {
-        width: 38%;
-      }
-      .all-details-layer {
-        width: 22%;
-      }
-    }
   }
   .workshop-info-banner,
   .layer {
