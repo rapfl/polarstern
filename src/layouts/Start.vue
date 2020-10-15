8fb6245b4904e7516aed1df7ab6527d77c164a58
Anton <e1127711@student.tuwien.ac.at>
2020-10-15 11:24:08 +0200

    New Header Design

--- src/layouts/Start.vue
+++ src/layouts/Start.vue
@@ -53,7 +53,7 @@
 
 <script>
   import Start from '~/data/Start.yml'
-  import Header from '~/components/Header.vue'
+  import Header from '~/components/NewHeader.vue'
 
   export default {
     components: {
